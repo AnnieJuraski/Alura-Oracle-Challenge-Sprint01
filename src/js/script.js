5b8e3128ce992b07a106779b4fd61fb7b396489d
Annie Alves <annie.a.alves@gmail.com>
2023-05-15 14:52:35 -0300

    Parte visual sem responsividade finalizada

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,55 @@
 const themeOpen = document.getElementById('theme-opener');
-const body = document.getElementsByTagName('body');
+const themeClose = document.getElementById('theme-closer')
+const body = document.querySelector('body');
+const themeMenu = document.querySelector('.themes-container');
+const toggler = document.querySelector('.toggler');
+const indicator = document.getElementById('indicator');
+const colorPicker = document.querySelectorAll('.color-btn');
 
-themeOpen.addEventListener('click', function () {
-    document.body.style.setProperty('--mainColor', 'var(--purple)');
 
+
+const savedTheme = localStorage.getItem('theme');
+if(savedTheme){
+    body.classList.add(savedTheme);
+    indicator.classList.toggle('active');
+}
+
+const savedColor = localStorage.getItem('color');
+if(savedColor){
+    document.querySelector(':root').style.setProperty('--mainColor', savedColor);
+}
+
+
+toggler.addEventListener('click', function () {
+    body.classList.toggle('light-theme');
+    indicator.classList.toggle('active');
+
+    const currentTheme = body.classList.contains('light-theme')? 'light-theme' : '';
+    localStorage.setItem('theme', currentTheme);
 })
+
+
+
+
+colorPicker.forEach(color => {
+    color.addEventListener('click', function(){
+
+        const dataColor = this.getAttribute('data-color');       
+
+        document.querySelector(':root').style.setProperty('--mainColor', dataColor);
+
+        localStorage.setItem('color', dataColor);
+    })
+})
+   
+    
+
+themeOpen.addEventListener('click', function () {    
+    themeMenu.classList.add('active');
+    body.style.paddingRight = '350px';
+})
+
+ themeClose.addEventListener('click', function () {
+    themeMenu.classList.remove('active');
+    body.style.paddingRight = '0px';
+ })
