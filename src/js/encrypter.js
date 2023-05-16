59e3d0d23f49479f8093955676676ce16ce7abf7
Annie Alves <annie.a.alves@gmail.com>
2023-05-16 10:57:12 -0300

    trdpondividade alterada

---
+++ src/js/encrypter.js
@@ -0,0 +1,26 @@
+const textArea = document.querySelector('.text-area');
+const message = document.querySelector('msg');
+
+const encryptButton = document.querySelector('.btn-encoder');
+const decryptButton = document.querySelector('.btn-decoder');
+
+
+encryptButton.addEventListener('click', function () {
+    let plainText = textArea.value;  
+})
+
+function encrypt(text) {
+    text = text.toLowerCase();
+}
+
+
+
+
+
+const alphabet = [
+    'A','B','C','D','E','F',
+    'G','H','I','J','K','L',
+    'M','N','O','P','Q','R',
+    'S','T','U','V','W','X',
+    'Y','Z'
+  ];
